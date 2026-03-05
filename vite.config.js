e8fdcd5c9d93aa18bc64ab7a48983303d2bb3235
emilirol <150665592+emilirol@users.noreply.github.com>
2026-03-05 14:51:58 +0100

    ...

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/emilierolstadsulen/",
+  plugins: [
+      react()
+    ],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
 });
 
