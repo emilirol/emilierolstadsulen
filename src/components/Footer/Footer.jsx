160ce60a4ac364d85cb4391dea4ccfefa26f6e94
emilirol <150665592+emilirol@users.noreply.github.com>
2026-03-24 13:44:31 +0100

    Småpirk

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,8 +12,8 @@ export default function Footer(){
 
             <div className="site__footer--contact">
                 <a href="mailto:emilie_rolstad@hotmail.com"><TbMailHeart className="react__icon"/></a>
-                <a href="https://github.com/emilirol"><FaGithub className="react__icon"/></a>
-                <a href="https://linkedin.com/in/emilie-sulen-788778248"><FaLinkedin className="react__icon"/></a>
+                <a href="https://github.com/emilirol" target="_blank"><FaGithub className="react__icon"/></a>
+                <a href="https://linkedin.com/in/emilie-sulen-788778248" target="_blank"><FaLinkedin className="react__icon"/></a>
             </div>
         </footer>
     )
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -14,16 +14,19 @@ export default function Contact(){
                     <h1>Kontakt meg!</h1>
                         <p>Kjære du som har "forvillet" deg inn på denne nettsiden!</p>
                         <p>Har du et prosjekt eller en idé innen UX-design og/eller utvikling?
-                            Kontakt meg gjerne via sosiale medier eller send meg en mail for en uforpliktende samtale.
+                            Jeg hjelper deg gjerne med et eksisterende prosjekt eller om du er på utkikk etter noe helt nytt.
+                        </p>
+                        <p>
+                            Kontakt meg gjerne via sosiale media eller send meg en mail for en uforpliktende samtale.
                         </p>
                         <p>Ser fram til å høre fra deg!</p>
                     
                     <div className="contact__social">
                         <a href="mailto:emilie_rolstad@hotmail.com"><TbMailHeart className="react__icon"/></a>
                         
-                        <a href="https://github.com/emilirol"><FaGithub className="react__icon"/></a>
-                        <a href="https://linkedin.com/in/emilie-sulen-788778248"><FaLinkedin className="react__icon"/></a>
-                        <a href="http://instagram.com/emiliro/"><FaInstagram className="react__icon"/></a>
+                        <a href="https://github.com/emilirol" target="_blank"><FaGithub className="react__icon"/></a>
+                        <a href="https://linkedin.com/in/emilie-sulen-788778248" target="_blank"><FaLinkedin className="react__icon"/></a>
+                        <a href="http://instagram.com/emiliro/" target="_blank"><FaInstagram className="react__icon"/></a>
                     </div>
                 </div>
             <Footer />
